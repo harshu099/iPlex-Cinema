3ecd4ef5e057fd9c47c6b5ec9e8644d5797e882f
harshu099 <76814545+harshu099@users.noreply.github.com>
2021-06-20 20:23:16 +0530

    Initial Commit

---
+++ work.js
@@ -0,0 +1,10 @@
+// function fun() {
+//     alert("Sign In Successfully");
+// }
+
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("dvalue").innerHTML = m + "/" + d + "/" + y;
